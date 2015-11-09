8f759820aec7309a25200c857073b11afd7293d6
Miquel Moya Sanjuán <miquelmoyasan@hotmail.com>
2015-11-09 17:12:24 +0100

    DYN TODO

--- TEORIA/DynArray.h
+++ TEORIA/DynArray.h
@@ -1,7 +1,7 @@
-#include "Defs.h"
+
 #ifndef __DYNARRAY__
 #define __DYNARRAY__
-
+#include "Defs.h"
 
 template <class TYPE>
 class DynArray
@@ -17,20 +17,14 @@ public:
 	//CONSTRUCTOR COPIA
 	DynArray(const DynArray& dyn) : num_elements(dyn.num_elements)
 	{
-		capacity = MAX(BLOCK, dyn.capacity);
+		capacity = MAX(BLOCK, num_elements);
 		data = new TYPE[capacity];
 		memcpy(data, dyn.data, dyn.num_elements*sizeof(TYPE));
 	}
 	//CONSTRUCTOR RESERVA MEMORIA
-	DynArray(TYPE& mem_res)
+	DynArray(uint mem_res) : capacity(MAX(mem_res, BLOCK))
 	{
-		tmp = new TYPE[capacity];
-		memcpy(data, tmp, num_elements*sizeof(TYPE));
-		delete[] data;
-		capacity += mem_res;
 		data = new TYPE[capacity];
-		memcpy(tmp, data, num_elements*sizeof(TYPE));
-		delete[] tmp;
 	}
 	//DESTRUCTOR
 	~DynArray()
@@ -42,22 +36,16 @@ public:
 	//AÑADE 1 CARACTER
 	void PushBack(const TYPE& data)
 	{
-		if (capacity > num_elements)
+		if (capacity < num_elements)
 		{
-			this->data[num_elements++] = data;
-		}
-		else
-		{
-			tmp = new TYPE[capacity];
-			memcpy(data, tmp, num_elements*sizeof(TYPE));
-			delete[] tmp;
+			TYPE* tmp = data;
 			capacity = capacity + BLOCK;
 			data = new TYPE[capacity];
-			memcpy(tmp, data, num_elements*sizeof(TYPE));
-			data[num_elements++] = tmp;
-			capacity = capacity + BLOCK;
+			memcpy(data, tmp, num_elements*sizeof(TYPE));
 			delete[] tmp;
 		}
+
+		this->data[num_elements++] = data;
 
 	}
 	//RETURN CAPACIDAD
@@ -85,5 +73,14 @@ public:
 	{
 		if (pos <= num_elements) ? data[pos] : NULL;
 	}
+	//TODO
+	/*
+	operator[]
+	operator=
+	pop_back()	(elimina ultim element)
+	srink_to_fit() (si sobra memoria es destrueix la k sobra)
+	flip()
+	insert(pos, data)   ABCDE -> INSERT(1, X) -> AXBCDE
+	*/
 };
 #endif
